9ed816c539d15ceed557dac6316b569cbe8e8147
Kavya Manupati <67407435+kavyamanu@users.noreply.github.com>
2022-04-17 20:17:44 +0530

    Initial setup and Readme (#1)

---
+++ utils/constants.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        id: "sign-up-form",
+        name: "Sign-up Form",
+        category: "HTML"
+    },
+    {
+        id: "rainbow-circle",
+        name: "Rainbow Circle",
+        category: "CSS"
+    },
+    {
+        id: "array-methods",
+        name: "Array Methods",
+        category: "JavaScript"
+}
+]
